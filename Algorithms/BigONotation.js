d3fed4b4ea683cfdfb0dc9ad599e4c6604665979
AbdullaPurpleplum <abdulla.agnigundala@purpleplum.design>
2023-08-31 22:29:48 +0530

    algorithms Time space notes

---
+++ Algorithms/BigONotation.js
@@ -0,0 +1,27 @@
+/* BIG O NOTATION*/
+/*
+1. The Worst case complexity of an algorithm is represented using the Big-O notaion
+2. Big-O notaion described the complexity of an algorithm using terms
+It has two important characteristics
+   1. It is expressed in terms of the input
+   2. It focuses on the bigger picture without getting caught up in the minute details
+*/
+
+
+/* Big O Time Complexity*/
+
+/*
+1. O(n) - Linear Time Complexity
+2. O(1) - Constant Time Complexity
+3. O(n^2) -Quantity Time Complexity
+4. O(n^3) - Cubic Time Complexity
+5, O(logn) - Logarthmic Time Complexity
+*/
+
+/*Big O Space Complexity*/
+
+/*
+1. O(1) - Linear Space Complexity
+2. O(n) - Constant Space Complexity
+O(logn) - Logarthmic Space Complexity
+*/
---
+++ Algorithms/TimeSpace.js
@@ -0,0 +1,10 @@
+/* How to represent complexity */
+
+/*
+Asymptotic notations
+-Mathematical tools to represent time and space complexity
+1. Big-O Notation (O - notation) - Worst case complexity 
+2. Omega Notation (-^- - notation) - Best case complexity
+3. Theta Notation (0 - notation) - Average case complexity
+*/
+
